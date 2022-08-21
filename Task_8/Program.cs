1b9d562c47145107e1f6af4bd2fc1481d3415515
Lazarev Konstantin <klik@example.com>
2022-08-21 14:41:54 +0300

    Все чётные числа от 1 до N

---
+++ Task_8/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+string first = Console.ReadLine();
+int N = Convert.ToInt32(first);
+int i = 2;
+Console.Write(N + " -> ");
+while(i <= N)
+{
+    Console.Write(i + ", ");
+    i += 2;    
+}
