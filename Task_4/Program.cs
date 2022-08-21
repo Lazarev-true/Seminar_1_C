e17d120f7d80f901eded7f5f613ad092db239efe
Lazarev Konstantin <klik@example.com>
2022-08-21 14:05:08 +0300

    Максимальное из трёх

---
+++ Task_4/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Ввод трёх чисел");
+Console.Write("Введите первое число (Десятичные через запятую): ");
+string first = Console.ReadLine();
+double firstnumber = Convert.ToDouble(first);
+ 
+Console.Write("Введите второе число (Десятичные через запятую): ");
+string second = Console.ReadLine();
+double secondnumber = Convert.ToDouble(second);
+
+Console.Write("Введите третье число (Десятичные через запятую): ");
+string third = Console.ReadLine();
+double thirdnumber = Convert.ToDouble(third);
+
+double max = firstnumber;
+
+if(secondnumber > max) max = secondnumber;
+if(thirdnumber > max) max = thirdnumber;
+
+Console.WriteLine("Максимальное из этих чисел: " + max);
