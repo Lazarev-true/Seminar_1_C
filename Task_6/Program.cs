5106b37ec029af534d8e480a1b468087807099be
Lazarev Konstantin <klik@example.com>
2022-08-21 14:23:23 +0300

    Чётное или нет

---
+++ Task_6/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число: ");
+string first = Console.ReadLine();
+int number = Convert.ToInt32(first);
+
+int N = number % 2;
+
+if(N == 0) Console.Write("Число чётное");
+if(N != 0) Console.Write("Число не чётное");
