e7c1d8691c1c709ccc7b1a46ce170269558bb4ab
Lazarev Konstantin <klik@example.com>
2022-08-21 14:08:45 +0300

    Максимальное и минимальное числа

---
+++ Task_2/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Ввод двух чисел");
+Console.Write("Введите первое число (Десятичные через запятую): ");
+string first = Console.ReadLine();
+double firstnumber = Convert.ToDouble(first);
+ 
+Console.Write("Введите второе число (Десятичные через запятую): ");
+string second = Console.ReadLine();
+double secondnumber = Convert.ToDouble(second);
+
+if(firstnumber > secondnumber)
+{
+    double max = firstnumber;
+    double min = secondnumber;
+    Console.WriteLine("Больше: " + max + " Меньше: " + min);
+}
+
+if(secondnumber > firstnumber)
+{
+    double max = secondnumber;
+    double min = firstnumber;
+    Console.WriteLine("Больше: " + max + " Меньше: " + min);
+}
+else
+{
+   Console.WriteLine("Числа равны"); 
+}
